47b20cf06a2b18f86b06c2475b32cc985e410c6c
mohamedtamer0 <mt4452242@gmail.com>
2021-11-08 20:33:15 +0200

    update

--- src/variables/main.kt
+++ src/variables/main.kt
@@ -1,2 +1,30 @@
 package variables
 
+fun main() {
+
+    val firstName2:String = "Ali"
+    //firstName2 = "ahmed"
+    println(firstName2.isEmpty())
+
+    val myAge:Int = 30
+    //myAge = 66
+    println(myAge)
+
+
+
+
+    println("=========================")
+
+
+    var firstName = "Mohamed"
+    firstName = "Ahmed"
+
+    println(firstName.isEmpty())
+
+    var lastName = "Tamer"
+    println(lastName.isEmpty())
+
+    var age = 25
+    println(age)
+
+}
