366bcd75034ef82b5723c1720084ab5d8c5e950b
mohamedtamer0 <mt4452242@gmail.com>
2021-11-09 23:18:15 +0200

    update

--- src/functions/example5.kt
+++ src/functions/example5.kt
@@ -1,25 +1,23 @@
 package functions
 
 fun main() {
-    //How to use Function Arguments in Kotlin
 //    person("Tamer",25)
-
-    person("Ahmed",25,false)
-    println("===============================")
-    person("Tamer",18,true)
-
+    person("Ahmed",18,true)
+    println("===============")
+    person("Tamer",25,false)
+    println("===============")
+    person("Mohab",18,false)
 }
 
 //fun person(name:String, age:Int) {
-//    println("$name, Your age is $age")
+//    println("$name, and your age is $age")
 //}
 
-fun person(name:String,age:Int, isHappy:Boolean) {
+fun person(name: String, age: Int, isHappy: Boolean) {
     if (age < 21)
-        println("your name is $name, and your age is $age")
+        println("Your name is $name, and your age is $age")
     else
         println("Sorry")
 
-    println("You are happy? $isHappy")
-
+    println("You are Happy? $isHappy")
 }
