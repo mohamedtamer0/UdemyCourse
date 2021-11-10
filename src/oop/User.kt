44629a5a654f9b118ab87f58989a69bf3742f2d3
mohamedtamer0 <mt4452242@gmail.com>
2021-11-10 21:48:59 +0200

    update

--- src/oop/User.kt
+++ src/oop/User.kt
@@ -1,18 +1,25 @@
 package oop
 
-class User {
-    var firstName: String = ""
-    var lastName:String = ""
+class User (var firstName: String,var lastName: String, var isPlatinum:Boolean) {
+
+    constructor(firstName: String,lastName: String):this(firstName,lastName,false) {
+        println("2nd")
+    }
+
+    constructor(firstName: String):this(firstName,"None") {
+        println("3rd")
+    }
+
 
     fun printFullName() {
         println("$firstName $lastName")
     }
 
-    fun printWithPrefix(prefix:String) {
+    fun printWithPrefix(prefix: String) {
         println("$prefix $firstName")
     }
 
-    fun updateName(newName:String) {
+    fun updateName(newName: String) {
         firstName = newName
     }
 
--- src/oop/main.kt
+++ src/oop/main.kt
@@ -2,25 +2,18 @@ package oop
 
 
 fun main() {
-    var user = User()
-    user.firstName = "Mohamed"
-    user.lastName = "Tamer"
+    var user = User("Mohamed","Tamer",true)
     user.printFullName()
-
     println("=========================")
 
-    var user2 = User()
-    user2.firstName = "Mohab"
-    user2.lastName = "Tamer"
+    var user2 = User("Mohab","Tamer")
     user2.printFullName()
     user2.updateName("Mohand")
     user2.printWithPrefix("Mr,")
     user2.firstNameLength()
     println("=========================")
 
-    var user3 = User()
-    user3.firstName = "Habiba"
-    user3.lastName = "Tamer"
+    var user3 = User("Habiba")
     user3.printFullName()
     user3.printWithPrefix("Ms,")
     user3.firstNameLength()
