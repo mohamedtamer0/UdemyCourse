932fdb46a335d3095a66adb89c106e9964404a8c
mohamedtamer0 <mt4452242@gmail.com>
2021-11-10 18:43:57 +0200

    update

--- src/nullSafety/main.kt
+++ src/nullSafety/main.kt
@@ -1,15 +1,14 @@
 package nullSafety
 
-import functions.person
-
 fun main() {
+
 //    var name:String = "Tamer"
 //    name = null
+//    println(name)
 
-    var name2: String? = "Tamer"
-    name2 = null
-    println(name2)
-
+    var name: String? = "Tamer"
+    name = null
+    println(name)
 
 //    var age:Int = 25
 //    age = null
@@ -18,25 +17,29 @@ fun main() {
     age = null
     println(age)
 
+
     println("==============================================")
 
     //Safe calls ?.
 
-    println("Safe calls ?.")
-    var firstName: String = "Mohamed"
-    var length = firstName.length
+    var firstName: String? = "Mohamed"
+    firstName = null
+    var length = firstName?.length
     println(length)
 
     println("==============================================")
 
     var lastName: String? = "Tamer"
-    lastName = null
     var length2 = lastName?.length
     println(length2)
+
     println("==============================================")
-    var length3 = if (lastName != null) lastName.length else 0
-    println(length3)
+
+    var lenght3 = if (lastName != null) lastName.length else 0
+    println(lenght3)
+
     println("==============================================")
+
     var length4 = lastName?.substring(0, 2)?.length
     println(length4)
 
@@ -46,26 +49,29 @@ fun main() {
     //Elvis operator ?:
     println("Elvis operator ?:")
 
-    var lastName2: String? = "Ali"
+    var lastName2: String? = "Mohamed Tamer"
     lastName2 = null
-    var length5: Int = if (lastName2 != null) lastName2.length else 0
+    var length5 = if (lastName2 != null) lastName2.length else 0
     println(length5)
 
-    var length6: Int = lastName2?.substring(0, 1)?.length ?: 0
+    var length6 = lastName2?.substring(0,4)?.length ?: 0
     println(length6)
-
 
     println("==============================================")
 
     //The !! operator
     println("The !! operator")
 
-    var name: String? = "Mohamed Tamer"
-    var length7:Int? = name!!.length
+    var name2:String? = "Mohamed Tamer"
+    var length7 = name2!!.length
     println(length7)
 
-    var sayHi:String? = null
-    var length8:Int? = sayHi?.length
+    var sayHi:String? = "Hi!"
+    var length8 = sayHi!!.length
+    println(length8)
+
+
+
 
 
 }
---
+++ src/oop/main.kt
@@ -0,0 +1,6 @@
+package oop
+
+
+fun main() {
+
+}
