9a2dff4cca0ee9c3f85691ae15cac49698ba5497
mohamedtamer0 <mt4452242@gmail.com>
2021-11-16 16:58:44 +0200

    update

--- src/functions/Lambda.kt
+++ src/functions/Lambda.kt
@@ -4,13 +4,26 @@ fun main() {
 
     // val lambdaName: (InputType) -> ReturnType = { arguments:InputType -> body }
     val greeting: (String) -> String = { name: String -> "Hello $name" }
-    val test = greeting("Mohamed")
+    val test = greeting("Tamer")
     println(test)
 
-
-    val greeting2 = { firstName: String,lastName:String ->
-        "Hello $firstName $lastName" }
+    val greeting2: (String, String) -> String = { firstName: String, lastName: String ->
+        "Hello $firstName $lastName"
+    }
     val test2 = greeting2("Mohamed","Tamer")
     println(test2)
+
+    val greeting3= { firstName: String, lastName: String ->
+        "Hello $firstName $lastName"
+    }
+    val test3= greeting3("Mohamed","Tamer")
+    println(test3)
+
+//    fun greeting(name: String): String{
+//        return "Hello $name"
+//    }
+//
+//    val test = greeting("Tamer")
+//    println(test)
 
 }
---
+++ src/functions/LambdaWithFunctionParameter.kt
@@ -0,0 +1,5 @@
+package functions
+
+fun main() {
+
+}
