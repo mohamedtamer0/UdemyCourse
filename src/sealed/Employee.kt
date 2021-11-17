9a98a96b402637b18c320ebf4ffccd0071bad8aa
mohamedtamer0 <mt4452242@gmail.com>
2021-11-18 01:08:06 +0200

    update

--- src/sealed/Employee.kt
+++ src/sealed/Employee.kt
@@ -4,3 +4,10 @@ sealed class Employee
 data class Manager(val name:String, val age:Int, val team:Int):Employee()
 class SeniorDev(val name:String, val age:Int, val projects:Int):Employee()
 object JuniorDev:Employee()
+
+
+//sealed class Employee {
+//    data class Manager(val name:String, val age:Int, val team:Int):Employee()
+//    class SeniorDev(val name:String, val age:Int, val projects:Int):Employee()
+//    object JuniorDev:Employee()
+//}
