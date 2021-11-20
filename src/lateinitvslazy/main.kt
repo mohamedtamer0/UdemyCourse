522a8337590b0ff8f0c1f9a774826f1dbf18a90c
mohamedtamer0 <mt4452242@gmail.com>
2021-11-20 22:51:29 +0200

    update

--- src/lateinitvslazy/main.kt
+++ src/lateinitvslazy/main.kt
@@ -1,2 +1,26 @@
 package lateinitvslazy
 
+fun main() {
+    //Lateinit VS Lazy
+
+//    val obj = MyInfo()
+//    println(obj.getValue())
+
+    val obj = MyInfo()
+    obj.setValue("Tamer")
+    println(obj.getValue())
+    println(obj.getValue())
+
+}
+
+class MyInfo() {
+//    val myName:String? = null
+    lateinit var myName:String
+
+    fun setValue(name:String) {
+        myName = name
+    }
+//    fun getValue() = myName
+
+    fun getValue() = if (::myName.isInitialized) myName else "Not initialized"
+}
