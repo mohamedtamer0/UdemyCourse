653f4a6ec76f213dc62698f648cabffafadad2e8
mohamedtamer0 <mt4452242@gmail.com>
2021-12-01 18:33:28 +0200

    update

--- src/anonymous/A.kt
+++ src/anonymous/A.kt
@@ -1,13 +1,27 @@
 package anonymous
 
-class A(val name:String){
-    fun printName() {
-        println("Hi $name")
-    }
+class A(val name:String) {
+//    fun printName() {
+//        println("Hi $name")
+//    }
 }
 
 
 fun main() {
-    val obj = A("Tamer")
-    obj.printName()
+
+    val helloWorld = object  {
+        val hello = "Hello"
+        val world = "World"
+        override fun toString() = "$hello $world"
+        val name = "Tamer"
+        fun printName() {
+            println("Hi $name")
+        }
+    }
+    println(helloWorld)
+    println(helloWorld.printName())
+
+
+//    val obj = A("Tamer")
+//    obj.printName()
 }
