5520347f98b6f0c9b5c2666d324f56ade5421da8
mohamedtamer0 <mt4452242@gmail.com>
2021-12-05 23:23:55 +0200

    update

--- src/problemSolvingPractice/problem2.kt
+++ src/problemSolvingPractice/problem2.kt
@@ -1,2 +1,30 @@
 package problemSolvingPractice
 
+/*
+Write a program that reads five words from the standard input and outputs the words in one line.
+ In the output, the words should be separated by a single space.
+ =====
+
+Sample Input:
+Never
+gonna
+give
+you
+up
+==================
+
+Sample Output:
+Never gonna give you up
+
+
+ */
+
+
+
+fun main() {
+
+
+    val word1 = readln()
+
+
+}
