f36740f96ee5d3087d681f3447d8cdc2f2a2b0f2
mohamedtamer0 <mt4452242@gmail.com>
2021-12-11 01:52:38 +0200

    update

---
+++ src/kotlinNewUpdate/standardlibrary/CollectionBuilders.kt
@@ -0,0 +1,6 @@
+package kotlinNewUpdate.standardlibrary
+
+
+fun main() {
+
+}
---
+++ src/problemSolvingPractice/Problem 3.kt
@@ -0,0 +1,30 @@
+package problemSolvingPractice
+/*
+Write a program that reads two strings and outputs them in the reverse order, each in a new line.
+=====================
+Sample Input:
+Kotlin
+Java
+
+======================
+Sample Output:
+Java
+Kotlin
+
+
+ */
+
+
+fun main() {
+
+    print(readln().also { println(readln()) })
+
+//    val a:List<String> = listOf(readln(), readln()).reversed()
+//    a.forEach { s -> println(s) }
+
+//    print(Array(2){ readln()}.reversed().joinToString("\n"))
+
+//    val (s1, s2) = List(2) { readln() }
+//    println("$s2\n$s1")
+
+}
