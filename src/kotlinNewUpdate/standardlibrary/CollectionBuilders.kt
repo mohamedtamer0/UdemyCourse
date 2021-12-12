6972a9b7870a3aae0d9537a98b99b3d7eec0f706
mohamedtamer0 <mt4452242@gmail.com>
2021-12-12 22:07:17 +0200

    update

--- src/kotlinNewUpdate/standardlibrary/CollectionBuilders.kt
+++ src/kotlinNewUpdate/standardlibrary/CollectionBuilders.kt
@@ -1,6 +1,14 @@
 package kotlinNewUpdate.standardlibrary
-
+import kotlin.random.Random
 
 fun main() {
-
+    val myList = buildList {
+        add("Kotlin")
+        add("Java")
+        addAll(listOf("PHP", "C++"))
+        set(1, "Python")
+        if (Random.nextBoolean()) add(0,"JavaScript")
+        reverse()
+    }
+    println(myList)
 }
